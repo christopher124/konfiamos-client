099d05900811c8df4a2c214f113d1ff083d406c0
Christopher Sosa <urri_@hotmail.com>
2023-04-22 19:28:10 -0600

    Envolver la app en authProvider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { BrowserRouter } from "react-router-dom";
 import { WebRouter, AdminRouter } from "./router";
+import { AuthProvider } from "./contexts";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <AdminRouter />
-      <WebRouter />
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <AdminRouter />
+        <WebRouter />
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
