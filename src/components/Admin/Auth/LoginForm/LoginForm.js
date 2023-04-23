71b6527bd0f6c0ba040227b71ca2f4f8bfba7a8e
Christopher Sosa <urri_@hotmail.com>
2023-04-23 17:41:36 -0600

    Recuperar el token en login y integracion de formik

--- src/components/Admin/Auth/LoginForm/LoginForm.js
+++ src/components/Admin/Auth/LoginForm/LoginForm.js
@@ -1,13 +1,45 @@
-import React from "react";
-
 import { Form } from "semantic-ui-react";
+import { useFormik } from "formik";
+import { Auth } from "../../../../api";
+import { useAuth } from "../../../../hooks";
+import { initialValues, validationSchema } from "./LoginForm.from";
+
+const authController = new Auth();
 
 export function LoginForm() {
+  const { login } = useAuth();
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: validationSchema(),
+    validateOnChange: false,
+    onSubmit: async (formValue) => {
+      try {
+        const response = await authController.login(formValue);
+        login(response.access);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  });
+
   return (
-    <Form>
-      <Form.Input name="email" placeholder="Correo electronico" />
-      <Form.Input name="password" type="password" placeholder="Contraseña" />
-      <Form.Button type="submit" primary fluid>
+    <Form onSubmit={formik.handleSubmit}>
+      <Form.Input
+        name="email"
+        placeholder="Correo electronico"
+        onChange={formik.handleChange}
+        value={formik.values.email}
+        error={formik.errors.email}
+      />
+      <Form.Input
+        name="password"
+        type="password"
+        placeholder="Contraseña"
+        onChange={formik.handleChange}
+        value={formik.values.password}
+        error={formik.errors.password}
+      />
+      <Form.Button type="submit" primary fluid loading={formik.isSubmitting}>
         Entrar
       </Form.Button>
     </Form>
