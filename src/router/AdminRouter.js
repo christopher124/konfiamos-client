78b509f5a56f48080179adff177e127381f64162
Christopher Sosa <urri_@hotmail.com>
2023-05-22 14:09:22 -0600

    Nuevas rutas para las paginas

--- src/router/AdminRouter.js
+++ src/router/AdminRouter.js
@@ -1,11 +1,10 @@
 import { Routes, Route } from "react-router-dom"; // rutas
 import { AdminLayout } from "../layouts"; // Layout del admin
-import { Auth } from "../pages/admin";
+import { Auth, Customer, Dashboard, Users } from "../pages/admin";
 import { useAuth } from "../hooks";
 
 export function AdminRouter() {
   const { user } = useAuth();
-  console.log(useAuth());
 
   // funcion para cargar layout de admin
   const loadLayout = (Layout, Page) => {
@@ -22,21 +21,21 @@ export function AdminRouter() {
       ) : (
         <>
           {/* Una pagina dos path */}
-          {["/admin", "/admin/blog"].map((path) => ({
-            /* <Route
-            key={path}
-            path={path}
-            element={loadLayout(AdminLayout, Blog)}
-          /> */
-          }))}
+          {["/admin", "/admin/dashboard"].map((path) => (
+            <Route
+              key={path}
+              path={path}
+              element={loadLayout(AdminLayout, Dashboard)}
+            />
+          ))}
 
-          {/* <Route path="/admin/users" element={loadLayout(AdminLayout, Users)} />
+          <Route path="/admin/users" element={loadLayout(AdminLayout, Users)} />
 
-        <Route
-          path="/admin/courses"
-          element={loadLayout(AdminLayout, Courses)}
-        />
-        <Route path="/admin/menu" element={loadLayout(AdminLayout, Menu)} />
+          <Route
+            path="/admin/customers"
+            element={loadLayout(AdminLayout, Customer)}
+          />
+          {/* <Route path="/admin/menu" element={loadLayout(AdminLayout, Menu)} />
         <Route
           path="/admin/newsletter"
           element={loadLayout(AdminLayout, Newsletter)}
