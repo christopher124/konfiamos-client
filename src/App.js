3cf9e95208b6c84827d3790ed5b77c7e6910d781
Christopher Sosa <urri_@hotmail.com>
2023-05-22 14:05:47 -0600

    Implementacion de react toastify en toda la aplicaccion

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import { BrowserRouter } from "react-router-dom";
 import { WebRouter, AdminRouter } from "./router";
 import { AuthProvider } from "./contexts";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function App() {
   return (
     <AuthProvider>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
       <BrowserRouter>
         <AdminRouter />
         <WebRouter />
