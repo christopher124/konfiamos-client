695d5e4b3762bc51b14c6b18b809d85d60bd9739
Christopher Sosa <urri_@hotmail.com>
2023-05-22 18:06:06 -0600

    Sistema de menu admin y otro roles

---
+++ src/components/Admin/AdminLayout/AdminMenu/AdminMenu.js
@@ -0,0 +1,67 @@
+import { Menu, Icon } from "semantic-ui-react";
+import { Link, useLocation } from "react-router-dom";
+import { useAuth } from "../../../../hooks";
+import "./AdminMenu.scss";
+
+export function AdminMenu() {
+  const { pathname } = useLocation();
+  const {
+    user: { role },
+  } = useAuth();
+  const isAdmin = role.name === "Admin";
+  const isCurrentPath = (path) => {
+    if (path === pathname) return true;
+    return false;
+  };
+
+  console.log(pathname);
+  return (
+    <Menu fluid vertical icon text className="admin-menu">
+      {isAdmin && (
+        <>
+          <Menu.Item
+            as={Link}
+            to="/admin/dashboard"
+            active={isCurrentPath("/admin/dashboard")}
+          >
+            <Icon name="dashboard" />
+            Dashboard
+          </Menu.Item>
+          <Menu.Item
+            as={Link}
+            to="/admin/users"
+            active={isCurrentPath("/admin/users")}
+          >
+            <Icon name="user" />
+            Usuarios
+          </Menu.Item>
+          <Menu.Item
+            as={Link}
+            to="/admin/customers"
+            active={isCurrentPath("/admin/customers")}
+          >
+            <Icon name="users" />
+            Clientes
+          </Menu.Item>
+          <Menu.Item
+            as={Link}
+            to="/admin/loanrequest"
+            active={isCurrentPath("/admin/loanrequest")}
+          >
+            <Icon name="pencil alternate" />
+            Prestamos
+          </Menu.Item>
+        </>
+      )}
+
+      <Menu.Item
+        as={Link}
+        to="/admin/menu"
+        active={isCurrentPath("/admin/menu")}
+      >
+        <Icon name="bars" />
+        Menu
+      </Menu.Item>
+    </Menu>
+  );
+}
