913ee44fa9d0764876ac2d7dc915066bef6fcf37
Christopher Sosa <urri_@hotmail.com>
2023-05-22 18:07:06 -0600

    Nuevas paginas de la aplicaccion

---
+++ src/pages/admin/LoanRequest/LoanRequest.js
@@ -0,0 +1,7 @@
+export function LoanRequest() {
+  return (
+    <div>
+      <h1>Estamos en LoanRequest</h1>
+    </div>
+  );
+}
--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -2,3 +2,5 @@ export * from "./auth";
 export * from "./Dashboard";
 export * from "./Customer";
 export * from "./Users";
+export * from "./Menu";
+export * from "./LoanRequest";
