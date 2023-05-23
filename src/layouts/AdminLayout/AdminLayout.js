8b397fcba4b89a6e6b5e53fbe73b22bd4482cff9
Christopher Sosa <urri_@hotmail.com>
2023-05-22 18:06:37 -0600

    Modificaciones en layaout

--- src/layouts/AdminLayout/AdminLayout.js
+++ src/layouts/AdminLayout/AdminLayout.js
@@ -1,16 +1,28 @@
-import React from "react";
+import { useState } from "react";
+import { Icon } from "../../assets";
+import { AdminMenu } from "../../components/Admin/AdminLayout";
+import { AiOutlineMenuFold, AiOutlineMenuUnfold } from "react-icons/ai";
 import "./AdminLayout.scss";
 
 export function AdminLayout(props) {
-  const { children } = props; // hijo de un componente
+  const { children } = props; // hijo del componente
+  const [isLeftVisible, setIsLeftVisible] = useState(true);
+
+  const toggleLeftVisibility = () => {
+    setIsLeftVisible(!isLeftVisible);
+  };
+
   return (
-    <div className="admin-layout">
-      <div className="admin-layout__left">
-        <div className="logo">LOGO</div>
-        <span>ADMIN Menu</span>
+    <div className={`admin-layout ${isLeftVisible ? "" : "left-hidden"}`}>
+      <div className={`admin-layout__left ${isLeftVisible ? "" : "hidden"}`}>
+        <Icon.LogoWhite className="logo" />
+        <AdminMenu />
       </div>
       <div className="admin-layout__right">
         <div className="admin-layout__right-header">
+          <button className="toggle-left-button" onClick={toggleLeftVisibility}>
+            {isLeftVisible ? <AiOutlineMenuFold /> : <AiOutlineMenuUnfold />}
+          </button>
           <span>LOGOUT</span>
         </div>
         <div className="admin-layout__right-content">{children}</div>
