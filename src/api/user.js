440e37f23661135fd9fa20adf31bfdfae3fc90dc
Christopher Sosa <urri_@hotmail.com>
2023-05-28 10:51:47 -0600

    Api crear usuario

--- src/api/user.js
+++ src/api/user.js
@@ -19,4 +19,37 @@ export class User {
       throw error;
     }
   }
+
+  async createUser(accessToken, data) {
+    try {
+      // para controlar imagenes
+      const formData = new FormData();
+      //para obtener los valores de los datos
+      Object.keys(data).forEach((key) => {
+        formData.append(key, data[key]);
+      });
+
+      if (data.fileAvatar) {
+        formData.append("avatar", data.fileAvatar);
+      }
+
+      const url = `${this.baseApi}/${ENV.API_ROUTES.USER}`;
+      const params = {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+        body: formData,
+      };
+
+      const response = await fetch(url, params);
+      const result = await response.json();
+
+      if (response.status !== 201) throw result;
+
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
 }
