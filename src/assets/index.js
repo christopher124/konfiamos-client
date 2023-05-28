da07deb237c3c9760abcdc3e636566f378207ee9
Christopher Sosa <urri_@hotmail.com>
2023-05-28 10:51:33 -0600

    Configuracion de fotos

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,17 @@
 import { ReactComponent as LogoWhite } from "./svg/LogoWhite.svg";
+
+import authBg from "./jpg/auth-bg.jpg";
+import homeBanner from "./jpg/home-banner.jpg";
+import noAvatar from "./jpg/no-avatar.jpg";
+
 const Icon = {
   LogoWhite,
 };
 
-export { Icon };
+const image = {
+  authBg,
+  homeBanner,
+  noAvatar,
+};
+
+export { Icon, image };
