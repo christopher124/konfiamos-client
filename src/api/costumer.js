2788a4f7a78638acc09ee846f973c245eb2999e7
Christopher Sosa <urri_@hotmail.com>
2023-05-31 16:36:32 -0600

    Api clientes

---
+++ src/api/costumer.js
@@ -0,0 +1,60 @@
+import { ENV } from "../utils";
+
+export class Customer {
+  baseApi = ENV.BASE_API;
+
+  async createCustomer(accessToken, data) {
+    try {
+      // para controlar imagenes
+      const formData = new FormData();
+      //para obtener los valores de los datos
+      Object.keys(data).forEach((key) => {
+        formData.append(key, data[key]);
+      });
+
+      if (data.fileAccountStatus) {
+        formData.append("accountStatus", data.fileAccountStatus);
+      }
+      if (data.fileIdentification) {
+        formData.append("identification", data.fileIdentification);
+      }
+
+      const url = `${this.baseApi}/${ENV.API_ROUTES.CUSTOMER}`;
+      const params = {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+        body: formData,
+      };
+
+      const response = await fetch(url, params);
+      const result = await response.json();
+
+      if (response.status !== 201) throw result;
+
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+  async getCustomers(accessToken, status = undefined) {
+    try {
+      const url = `${this.baseApi}/${ENV.API_ROUTES.CUSTOMERS}?status=${status}`;
+      const params = {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      };
+
+      const response = await fetch(url, params);
+      const result = await response.json();
+
+      if (response.status !== 200) throw result;
+
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
