27c43c4b043d0a5dd4c0d4133949d8d5e46e5451
Christopher Sosa <urri_@hotmail.com>
2023-05-31 10:22:58 -0600

    Componente list users

--- src/components/Admin/Users/ListUsers/ListUsers.js
+++ src/components/Admin/Users/ListUsers/ListUsers.js
@@ -1,13 +1,14 @@
 import { useState, useEffect } from "react";
 import { Loader } from "semantic-ui-react";
-import { size } from "lodash";
+import { size, map } from "lodash";
 import { User } from "../../../../api";
 import { useAuth } from "../../../../hooks";
+import { UserItem } from "../UserItem";
 
 const userController = new User();
 
 export function ListUsers(props) {
-  const { usersActive } = props;
+  const { usersActive, reload } = props;
   const [users, setUsers] = useState(null);
   const { accessToken } = useAuth();
   console.log(users);
@@ -25,15 +26,45 @@ export function ListUsers(props) {
         console.log(error);
       }
     })();
-  }, [usersActive]);
+  }, [usersActive, reload]);
 
   if (!users) return <Loader active inline="centered" />;
   if (size(users) === 0) return "No hay ningun usuario";
 
   return (
-    <div>
-      <h2>Estamos viendo los usuarios</h2>
-      <p>{usersActive ? "Activos" : "Inactivos"}</p>
+    <div className="relative overflow-x-auto shadow-2xl sm:rounded-lg">
+      <table
+        id="tableDoctors"
+        className=" border border-slate-50 w-full  text-base text-center text-white"
+      >
+        <thead className="border-b text-base uppercase bg-[#687584] text-white">
+          <tr className="border border-slate-50">
+            <th scope="col" className=" text-white py-3 px-6 text-center ">
+              Avatar
+            </th>
+            <th scope="col" className=" text-white py-3 px-6 text-center ">
+              Nombres completo
+            </th>
+            <th scope="col" className="text-white py-3 px-6 text-center">
+              Correo
+            </th>
+            <th scope="col" className="text-white py-3 px-6 text-center">
+              Rol
+            </th>
+            <th
+              scope="col"
+              className="text-white font-bold py-3 px-6 text-center "
+            >
+              Acciones
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {map(users, (user) => (
+            <UserItem key={user._id} user={user} />
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
