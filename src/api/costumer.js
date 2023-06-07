5772fb16e34731621980611063be139b76166896
Christopher Sosa <urri_@hotmail.com>
2023-06-07 16:52:25 -0600

    Api clientes updateCustomer

--- src/api/costumer.js
+++ src/api/costumer.js
@@ -38,6 +38,43 @@ export class Customer {
       throw error;
     }
   }
+
+  async updateCustomer(accessToken, idCustomer, costomerData) {
+    try {
+      const data = costomerData;
+      // para controlar imagenes
+      const formData = new FormData();
+      //para obtener los valores de los datos
+      Object.keys(data).forEach((key) => {
+        formData.append(key, data[key]);
+      });
+
+      if (data.fileAccountStatus) {
+        formData.append("accountStatus", data.fileAccountStatus);
+      }
+      if (data.fileIdentification) {
+        formData.append("identification", data.fileIdentification);
+      }
+      const url = `${ENV.BASE_API}/${ENV.API_ROUTES.CUSTOMER}/${idCustomer}`;
+      const params = {
+        method: "PATCH",
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+        body: formData,
+      };
+
+      const response = await fetch(url, params);
+      const result = await response.json();
+
+      if (response.status !== 200) throw result;
+
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+
   async getCustomers(accessToken, status = undefined) {
     try {
       const url = `${this.baseApi}/${ENV.API_ROUTES.CUSTOMERS}?status=${status}`;
