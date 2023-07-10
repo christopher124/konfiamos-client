9e4a8054ab42645de48f967a6635d344e373e7e7
Christopher Sosa <urri_@hotmail.com>
2023-07-09 19:02:15 -0600

    Api de recuperar un prestamo

--- src/api/loanrequest.js
+++ src/api/loanrequest.js
@@ -22,6 +22,25 @@ export class LoanRequest {
     }
   }
 
+  async getLoanRequest(accessToken, idLoanRequest) {
+    try {
+      const url = `${this.baseApi}/${ENV.API_ROUTES.LOANREQUEST}/${idLoanRequest}`;
+      const params = {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      };
+      const response = await fetch(url, params);
+      const result = await response.json();
+
+      if (response.status !== 200) throw result;
+
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+
   async createLoanRequest(accessToken, data) {
     const url = `${this.baseApi}/${ENV.API_ROUTES.LOANREQUEST}`;
     const params = {
