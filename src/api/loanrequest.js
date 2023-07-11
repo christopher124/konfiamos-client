a3f19403b1859a973ae19e0906478bc9e4cc831a
Christopher Sosa <urri_@hotmail.com>
2023-07-10 19:51:18 -0600

    Nueva api count invertido

--- src/api/loanrequest.js
+++ src/api/loanrequest.js
@@ -22,6 +22,25 @@ export class LoanRequest {
     }
   }
 
+  async getTotalInvestment(accessToken) {
+    try {
+      const url = `${this.baseApi}/${ENV.API_ROUTES.TOTALINVESTMENT}`;
+      const params = {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      };
+      const response = await fetch(url, params);
+      const result = await response.json();
+
+      if (response.status !== 200) throw result;
+
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+
   async getLoanRequest(accessToken, idLoanRequest) {
     try {
       const url = `${this.baseApi}/${ENV.API_ROUTES.LOANREQUEST}/${idLoanRequest}`;
